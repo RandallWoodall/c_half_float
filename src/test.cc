ddb19047743bcbcbfaa96dad9dac582dbbf1395f
RandallWoodall <randalljwoodall@gmail.com>
2018-07-02 18:12:41 -0600

    Storing float values into a file.

--- src/test.cc
+++ src/test.cc
@@ -6,14 +6,15 @@
 #include "bfloat16.hh"
 
 //Test the program by using a variable number of floats of each type
-//(float, bfloat16 (intel), half_float (ieee 745))
+//(float, bfloat16 (intel), half_float (ieee 745 2008))
 
 int main(int argc, char ** argv) {
     //Generate argv[0] float values, store them in a file
     srand(time(NULL));
-    float * setup = (float*)malloc(sizeof(float) * std::stoi(argv[1]));
     std::ofstream file;
     file.open("setup.dat");
     for(int i = 0; i < std::stoi(argv[1]); i++)
-        file << setup[i] << std::endl;
+        file << static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/5000)) << std::endl;
+    //All values are in the file, time pulling and calculation with float
+
 }
