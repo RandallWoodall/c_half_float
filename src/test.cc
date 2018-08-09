e697949a2656b33499ab5effae9df20025bee8f4
RandallWoodall <randalljwoodall@gmail.com>
2018-08-09 10:11:24 -0600

    Test now shows some significant differences between the standard float,double and the new half_float.

--- src/test.cc
+++ src/test.cc
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <cstdlib>
 #include <ctime>
+#include <string>
 #include "half_float.hh"
 #include "bfloat16.hh"
 
@@ -9,13 +10,25 @@
 //(float, bfloat16 (intel), half_float (ieee 745 2008))
 
 int main(int argc, char ** argv) {
-    //Generate argv[0] float values, store them in a file
+    //Generate argv[1] float values, store them in a file
     srand(time(NULL));
-    //std::ofstream file;
-    //file.open("setup.dat");
-    //for(int i = 0; i < std::stoi(argv[1]); i++)
-    //    file << static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/5000)) << std::endl;
-    //All values are in the file, time pulling and calculation with float
-    half_float value = half_float(1.5743);
-    std::cout << value.get_value() << std::endl;
+    std::ofstream file;
+    file.open("setup.dat");
+    for(int i = 0; i < std::stoi(argv[1]); i++)
+        file << static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/5000)) << std::endl;
+    file.close();
+    std::ifstream file2;
+    file2.open("setup.dat");
+    //All values are in the file, time pulling with float, double, half_float
+    float * floats = (float*) malloc(sizeof(float) * std::stoi(argv[1]));
+    double * doubles = (double*) malloc(sizeof(double) * std::stoi(argv[1]));
+    half_float * halfs = (half_float*) malloc(sizeof(half_float) * std::stoi(argv[1]));
+    for(int i = 0; i < std::stoi(argv[1]); i++) {
+        std::string get;
+        getline(file2, get);
+        floats[i] = std::stof(get);
+        doubles[i] = std::stod(get);
+        halfs[i] = half_float(std::stod(get));
+        std::cout << floats[i] << " " << doubles[i] << " " << halfs[i].get_value() << std::endl;
+    }
 }
